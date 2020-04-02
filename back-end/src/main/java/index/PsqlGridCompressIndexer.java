ce6968fbcb8393a8589b2571034452aa71dd93f7
Peter Griggs <pgriggs@mit.edu>
2020-04-01 18:56:52 -0700

    Changed grid compressor's grid cell size to 100

--- back-end/src/main/java/index/PsqlGridCompressIndexer.java
+++ back-end/src/main/java/index/PsqlGridCompressIndexer.java
@@ -19,8 +19,8 @@ import project.Transform;
 public class PsqlGridCompressIndexer extends BoundingBoxIndexer {
 
     private static PsqlGridCompressIndexer instance = null;
-    private final int gridW = 1000;
-    private final int gridH = 1000;
+    private final int gridW = 100;
+    private final int gridH = 100;
     private final int batchSize = 10000000;
     private final int updBatchSize = 5000;
 
